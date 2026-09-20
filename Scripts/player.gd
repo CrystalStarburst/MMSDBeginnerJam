873233862ff1e2f3e95a04b5eeff1f7af6103873
BadMonkey314 <aidan.kl.tang@gmail.com>
2026-09-21 00:26:02 +0800

    Dash added

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,14 +2,17 @@ extends CharacterBody2D
 
 @onready var player_sprite: AnimatedSprite2D = $PlayerSprite
 @onready var player_hitbox: CollisionShape2D = $PlayerHitbox
+@onready var dash_cd: Timer = $DashCD
+@onready var dash_frames: Timer = $DashFrames
 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const ACCELERATION = 500.0
-const DASH_SPEED = 1000.0
+const DASH_SPEED = 500.0
 
-
+var dashing: bool = false
+var can_dash: bool = false
 var facing: int = 1
 
 enum States {
@@ -26,12 +29,9 @@ var state: States = States.Idle
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and not dashing:
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -39,32 +39,48 @@ func _physics_process(delta: float) -> void:
 	var direction_x: int = Input.get_axis("left", "right")
 	@warning_ignore("narrowing_conversion")
 	var direction_y: int = Input.get_axis("up", "down")
-	if direction_x:
-		velocity.x = move_toward(velocity.x, SPEED*direction_x, ACCELERATION)
-		facing = direction_x
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	if is_on_floor() and state != States.Dashing:
-		if direction_x != 0:
-			state = States.Walking
+	if not dashing and not state == States.WallJumping:
+		if direction_x:
+			velocity.x = move_toward(velocity.x, SPEED*direction_x, ACCELERATION)
+			facing = direction_x
+			
 		else: 
-			state = States.Idle
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			if direction_x :
+				state = States.Walking
+			else:
+				state = States.Idle
+		elif is_on_wall_only():
+			state = States.WallSliding
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
 	
 	if Input.is_action_just_pressed("dash"):
-		state = States.Dashing
 		velocity = Vector2(direction_x, direction_y).normalized() * DASH_SPEED
+		facing = direction_x
+		state = States.Dashing
+		dashing = true
+		dash_frames.start()
+		dash_cd.start()
+		
 		
 	
 	player_sprite.set_flip_h(facing!=1)
 		
-	
+	print("state: ", state, "Velocity", velocity)
 	move_and_slide()
 
 
 func _on_dash_frames_timeout() -> void:
-	pass
+	velocity = velocity/2
+	dashing = false
+	
 
 
 func _on_dash_cd_timeout() -> void:
-	pass # Replace with function body.
+	can_dash = true
